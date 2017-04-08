d9cfd7c9ce9701a0f73efa6e58b94c8b6e9a2aad
David Liang <themarioman@gmail.com>
2017-04-08 17:58:55 -0400

    Icons added for upcoming functionality

--- app/src/main/java/dimensionalstudios/basicnotetaker/MainActivity.java
+++ app/src/main/java/dimensionalstudios/basicnotetaker/MainActivity.java
@@ -1,6 +1,8 @@
 package dimensionalstudios.basicnotetaker;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -12,7 +14,10 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 //Use loader interface to create asynchronous operations!
 
@@ -25,18 +30,20 @@ public class MainActivity extends AppCompatActivity implements android.app.Loade
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        insertNote("New note");
-        //query notes table
-        //Cursor cursor = getContentResolver().query(noteProvider.CONTENT_URI,
-        //        dbOpenHelper.ALL_COLUMNS, null, null, null, null);
+        //insertNote("New note");
+
+        /*query notes table
+          Cursor cursor = getContentResolver().query(noteProvider.CONTENT_URI,
+                  dbOpenHelper.ALL_COLUMNS, null, null, null, null);*/
+
         //Where data is coming from
         String[] from = {dbOpenHelper.NOTE_TEXT};
         //ID of text view that will be used in a view that is delivered by the SDK
         //What will display with
-        int[] to = {android.R.id.text1};
-
+        //int[] to = {android.R.id.text1};
+        int[] to = {R.id.tvNote};
         cursorAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, null, from, to);
+                    R.layout.note_list_item, null, from, to, 0);
 
         ListView list = (ListView) findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
@@ -55,6 +62,65 @@ public class MainActivity extends AppCompatActivity implements android.app.Loade
 
         //Debug
         Log.d("MainActivity", "Inserted note"+noteUri.getLastPathSegment());
+    }
+
+    //Menu and menu options
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNodes();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Test methods
+    private void insertSampleData() {
+        insertNote("First");
+        insertNote("Multi-line\nnote");
+        insertNote("A super long node that may or may not wrap the bounds of the application window but with tablets this may not happen");
+        restartLoader();
+
+    }
+
+    private void deleteAllNodes() {
+        //Confirm? Dialogue box
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //delete all
+                            getContentResolver().delete(noteProvider.CONTENT_URI, null, null);
+                            restartLoader();
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void restartLoader() {
+        //Called every time we insert data
+        getLoaderManager().restartLoader(0,null,this);
     }
 
     //Loader methods
